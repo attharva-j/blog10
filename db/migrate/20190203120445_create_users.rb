7dc215d4c69d423e1e1e0bc510b5483e0b8e76ac
attharva-j <athex25@gmail.com>
2019-02-15 23:11:26 +0530

    First Ruby on Rails Project on Comments

---
+++ db/migrate/20190203120445_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.date :dob
+      t.string :email
+      t.string :gender
+
+      t.timestamps
+    end
+  end
+end
